2daf4992129ea52389fa10734e459f5aa95e4429
Logan Gingerich <logangingerich@yahoo.com>
2017-02-02 20:28:12 -0700

    Checkpoint 2:Complete; Initialized and auth token recieved/stored

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,5 +1,13 @@
-Class Kele
-  def initialize(username, password)
+require "httparty"
 
+class Kele
+  include HTTParty
+
+  def initialize(username, password)
+    @bloc_base_api_url = "https://www.bloc.io/api/v1"
+    @user_auth_token = self.class.post("https://www.bloc.io/api/v1/sessions",
+                       body: {email: username, password: password})
+    raise "Wrong Email and/or Password" if @user_auth_token.code != 200
   end
+
 end
