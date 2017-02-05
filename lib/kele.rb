66fa8708248f656b92ab69e64926e95b701f3314
Logan Gingerich <logangingerich@yahoo.com>
2017-02-04 17:25:15 -0700

    Checkpoint 3 Complete: get_me retrieves my data and converts to a hash

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,13 +1,20 @@
 require "httparty"
+require "json"
 
 class Kele
   include HTTParty
 
   def initialize(username, password)
     @bloc_base_api_url = "https://www.bloc.io/api/v1"
-    @user_auth_token = self.class.post("https://www.bloc.io/api/v1/sessions",
+    response = self.class.post("https://www.bloc.io/api/v1/sessions",
                        body: {email: username, password: password})
-    raise "Wrong Email and/or Password" if @user_auth_token.code != 200
+    @auth_token = response["auth_token"]
+    raise "Wrong Email and/or Password" if response.code != 200
+  end
+
+  def get_me
+    response = self.class.get("https://www.bloc.io/api/v1/users/me", headers: { "authorization" => @auth_token })
+    @my_data = JSON.parse(response.body)
   end
 
 end
